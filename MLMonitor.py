31f695e40ac32126e9c68b251abe1b62a807ef2a
Nicholas Toldalagi <ntoldalagi@gmail.com>
2021-10-15 07:33:28 -0400

    Adding timing reporter

--- MLMonitor.py
+++ MLMonitor.py
@@ -1,6 +1,8 @@
 from twilio.rest import Client
 import monitor_credentials as mc
 
+import time
+
 def report_epoch(epoch, model_name, total_epochs=None, loss=None, accuracy=None, time=None, time_units='Secs'):
     msg = '{mn} UPDATE: \n'.format(mn=model_name.upper())
     msg += 'Epoch: {e}'.format(e=epoch)
@@ -23,4 +25,23 @@ def report_epoch(epoch, model_name, total_epochs=None, loss=None, accuracy=None,
     client = Client(mc.ACCOUNT_SID, mc.AUTH)
     client.messages.create(mc.PERSONAL_NUMBER, from_=mc.TWILIO_NUMBER, body=msg)
 
-report_epoch(3, 'model_1', total_epochs=22, loss=3.252523, accuracy=.135825, time=154.289)
+#report_epoch(3, 'model_1', total_epochs=22, loss=3.252523, accuracy=.135825, time=154.289)
+
+class reporter:
+    def __init__(self, total_steps):
+        self.curr_avg_time = 0.0
+        self.curr_steps = 0
+        self.total_steps = 0
+
+    def begin_timing(self):
+        self.last_time = time.time()
+
+    def time_step(self):
+        self.curr_steps += 1
+        time_delta = time.time() - self.last_time
+        self.curr_avg_time = (self.curr_avg_time*(self.curr_steps-1) + time_delta)/(self.curr_steps)
+        time_left = (self.total_steps - self.curr_steps) * self.curr_avg_time 
+        print('EPOCH TIME [{}]s ESTIMATED TIME LEFT [{}]s'.format(self.curr_avg_time, time_left))
+
+
+
